0349cb08ada294de05310ac1ddb6d049bbaa34dc
Mwmuraguri <125728086+Mwmuraguri@users.noreply.github.com>
2024-03-27 10:49:57 +0300

    Update settings.py

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-e@*ejw2(q7e%!b^bcq-q&&18)pe!tcqzyhw0s=+4mr#ii7_zf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'Mwngaruiya.pythonanywhere.com']
 
 
 # Application definition
@@ -121,5 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
